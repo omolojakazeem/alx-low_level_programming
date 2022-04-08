df7cfe95cc07d7083ec9905d146d67da731bd816
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-07 17:58:00 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	cout("and that piece of art is useful\" - Dora Kprpar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Kprpar, 2015-10-19\n");
 
 	return (1);
 }
