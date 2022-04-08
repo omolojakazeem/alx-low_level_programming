b1c3cf8c3267285097d757bdff0746b6c11935fb
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-08 15:23:50 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,13 +10,11 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit;
-       
 	last_digit = n % 10;
-
 	if (last_digit == 0)
 	{
 		printf("Last digit of %d is 0 and is 0\n", n);
