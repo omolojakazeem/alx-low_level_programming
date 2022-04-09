b06ca437f6671bee0ab7b574a7e6395705dacbb7
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-09 05:19:42 -0700

    Print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
