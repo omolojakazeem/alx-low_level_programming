74223df624d2a541cf317d9e9614026114ce42c2
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-09 02:37:08 -0700

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a == 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	for (b == 'A'; b <= 'F'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
