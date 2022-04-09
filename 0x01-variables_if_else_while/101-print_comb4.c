6106438d063a29d0fb03f487f3a86287625b0e6a
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-09 07:08:13 -0700

    Print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 55; a++)
+	{
+		for (b = 49; b <= 56; b++)
+		{
+			if (a != b && a < b)
+			{
+				for (c = 50; c <= 57; c++)
+				{
+					if (b != c && b < c)
+					{
+						putchar(a);
+						putchar(b);
+						putchar(c);
+						if (a + b + c != 168)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
