3bf18c489521a1f028a43c80990c39a0cfd1c483
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-13 15:55:46 -0700

    Print Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.c"
+
+/**
+ * print_sign - Print the sign of an input
+ * @n: takes integer n
+ *
+ * Return: 1 if +, 0 if -
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar(48 + 0);
+		return (0);
+	}
+	else
+	{
+		if (n > 0)
+		{
+			_putchar(43 + 0);
+			return (1);
+		}
+		else
+		{
+			_putchar(45 + 0);
+			return (-1);
+		}
+	}
+}
