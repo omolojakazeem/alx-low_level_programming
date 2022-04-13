bbb857e65257dc68b98ad821f584b5910f26a045
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-12 20:15:59 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Main - Entry point
+ * main - Entry point
  *
  * Description: Prints _putchar using putchar prototypr
  *
@@ -12,7 +12,7 @@ int main(void)
 	char ch[8] = "_putchar";
 	int i = 0;
 
-	while (i <= 8)
+	while (i <= 7)
 	{
 		_putchar(ch[i]);
 		i++;
