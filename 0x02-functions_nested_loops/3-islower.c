7c1a818bbdfd6988d7d83689c18312db0ab85c34
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-12 21:29:38 -0700

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if an input is lowercase
+ *
+ * return: 0 - is lower 1 - if not
+ */
+int _islower(int i)
+{
+	if (i >= 97 && i <= 122)
+	{ 
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
