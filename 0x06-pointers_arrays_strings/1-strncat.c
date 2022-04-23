42c15b2e52ca1073950c21d5fd3d11d2941ab346
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-22 18:44:29 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+/**
+ * _strncat - function that concantenates n number of strings
+ *
+ * @dest: pointer to destination inpute
+ * @src: pointer to source input
+ * @n: most number of bytes from the source
+ *
+ * Return - @dest address
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int csize, i;
+
+	csize = 0;
+
+	while (dest[csize])
+	{
+		csize++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[csize + 1] = src[i];
+	}
+	dest[csize + 1] = '\0';
+
+	return (dest);
+}
