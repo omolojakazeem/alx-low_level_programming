9cab88b6b8c4b2c8c16282ee0759fd3c694b9869
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-23 09:52:22 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a tring in rot13
+ *
+ * @s: pointer to the string to encode
+ *
+ * Return: the result
+ */
+
+char *rot13(char *s)
+{
+	int x, y;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; a[y] != '\0'; y++)
+		{
+			if (s[x] == a[y])
+			{
+				s[x] = b[y];
+				break;
+			}
+		}
+	}
+	return (s);
+}
