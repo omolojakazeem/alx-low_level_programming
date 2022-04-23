9d3bb2c17101ddf32dc4afd9a3860a7e18c1b122
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-22 19:05:06 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+/**
+ * _strcmp - function to compare 2 strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 0 if s1 and 2 are same
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, r;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			r = s1[i] - s2[i];
+		}
+		i++;
+	}
+	return (r)
+}
