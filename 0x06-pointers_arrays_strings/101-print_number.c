903f40ee73effd9823c7d6fa3ae7f5b132fd105c
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-23 10:03:26 -0700

    print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer (Number)
+ *
+ * @n: integer to be printed
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
