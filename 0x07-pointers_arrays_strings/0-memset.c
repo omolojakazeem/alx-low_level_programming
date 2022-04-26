2ab7a232718c375bbf51be9bd75cfbd1a157d2e6
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-26 10:03:57 -0700

    Set memory val

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+/**
+ * _memset - function to fill memory with a constant byte
+ *
+ * @s: input to char type data
+ * @b: inpute variable of char type
+ * @n: unsigned int variable rep the number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	while (i < n)
+	{
+		s[i] = b;
+		++i;
+	}
+	return (s);
+}
