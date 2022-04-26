f4f6edbabefa148973ae9e324e6664403bc88b32
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-26 10:38:49 -0700

    Search char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,10 +16,14 @@ char *_strchr(char *s, char c)
 		{
 			return (s);
 		}
-		else
-		{
-			return ('\0');
-		}
 		++s;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	else
+	{
+		return ('\0');
 	}
 }
