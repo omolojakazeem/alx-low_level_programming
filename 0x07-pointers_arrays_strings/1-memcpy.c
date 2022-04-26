7741fe30c7a9aeaccaf96ddf93e3215182e00245
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-26 10:15:10 -0700

    Copy memory

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _memcpy - A function that copies memory area
+ *
+ * @dest: pointer to destination address
+ * @src: pointer to source address
+ * @n: number of char to copy
+ *
+ * Return: dest
+ */
+
+char *_memcp(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
