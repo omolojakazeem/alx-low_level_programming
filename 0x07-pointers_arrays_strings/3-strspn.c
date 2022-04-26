31a08fbce01d69a034feda7fd7308b231c8dac7a
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-26 10:55:07 -0700

    Search substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+/**
+ *  _strspn - A function that returns the lenght of a substring
+ * @s: string pointet
+ * @accept: substring
+ *
+ * Return: number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		k = 1; /* used as flag */
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				k = 0;
+				break;
+			}
+			++j;
+		}
+		if (f == 1)
+		{
+			break;
+		}
+		++i;
+	}
+	return (i);
+}
