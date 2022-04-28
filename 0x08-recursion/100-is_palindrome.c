275839b3e628bb890a024d74b7fe17b4c6fac3d8
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-28 15:45:14 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,60 @@
+/**
+ * _strlen_recursion - function to get string lenght
+ * @s: string
+ * Return: Always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
+
+/**
+ * palindrome - function palindrome
+ *
+ * @s: chaine of the char
+ * @taille: the number
+ * @i: the numer
+ * Return: Always 0
+ */
+
+int palindrome(char *s, int taille, int i)
+{
+	if (s[i] != '\0')
+	{
+		if (s[i] != s[taille - 1])
+		{
+			return (0);
+		}
+		else
+		{
+			return (palindome(s, taille, i + 1));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * is_palindrome - fuction
+ * @s: the chaine of the char
+ * Return: Alwys 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return (1);
+	}
+	return (palindrome(s, _strlen_recursion(s) - 1, 0));
+}
