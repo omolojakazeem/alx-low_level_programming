37bf0f3e86b2b3da95c6352b7c97eeab964013f2
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-28 14:04:34 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function for recursion
+ *
+ * @s: the chain
+ * Return: Always (0) Success
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
