180d10084d48ec19cc137890acd18746785ce9e3
Omoloja Kazeem <omolojakazeem@gmail.com>
2022-04-28 14:24:49 -0700

    Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+/**
+ * factorial - Function
+ *
+ * @n: the number
+ * Return: Always 0 (success)
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
